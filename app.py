545d6fb35a7d3192af79e9531073124b542f1ceb
Zukhulento <luis.matus.jr@gmail.com>
2023-11-22 11:37:55 -0600

    login and register, also tailwind

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from cs50 import SQL
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+# Database config
+db = SQL("sqlite:///SmartPlanner.db")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # validating data
+        username = request.form['username']
+        password = request.form['password']
+        confirmpass = request.form['confirmation']
+        if not username:
+            return render_template('register.html', error='Username is required')
+        elif not password:
+            return render_template('register.html', error='Password is required')
+        elif not confirmpass:
+            return render_template('register.html', error='Confirm password is required')
+        elif password != confirmpass:
+            return render_template('register.html', error='Password and Confirm password do not match')
+        elif db.execute("SELECT * FROM users WHERE user = :username", username=username):
+            return render_template('register.html', error='Username already exists')
+        # encrypting password
+        hash = generate_password_hash(password)
+        # inserting data into database
+        db.execute("INSERT INTO users (user, password) VALUES (:username, :hash)", username=username, hash=hash)
+        # redirecting to login page
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+         # validating data
+        username = request.form['username']
+        password = request.form['password']
+        if not username:
+            return render_template('login.html', error='Username is required')
+        elif not password:
+            return render_template('login.html', error='Password is required')
+        row = db.execute("SELECT * FROM users WHERE user = :username", username=username)
+        if row:
+            # validating pass
+            if check_password_hash(row[0]["password"],password):
+                userData={"username":row[0]["user"],"id":row[0]["id"]}
+                return render_template('home.html',userData=userData)
+        return render_template('login.html', error='Incorrect credentials')
+        
+        return redirect(url_for('login'))
+    return render_template('login.html', error=error)
