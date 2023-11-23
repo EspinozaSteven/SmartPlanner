d24e992986998806d3d7bdb2b8346cef38d2ccdd
Zukhulento <luis.matus.jr@gmail.com>
2023-11-23 11:42:23 -0600

    app update with new bd

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from cs50 import SQL
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -15,23 +16,26 @@ def index():
 def register():
     if request.method == 'POST':
         # validating data
+        mail = request.form['email']
         username = request.form['username']
         password = request.form['password']
         confirmpass = request.form['confirmation']
         if not username:
             return render_template('register.html', error='Username is required')
+        elif not mail:
+            return render_template('register.html', error='Email is required')
         elif not password:
             return render_template('register.html', error='Password is required')
         elif not confirmpass:
             return render_template('register.html', error='Confirm password is required')
         elif password != confirmpass:
             return render_template('register.html', error='Password and Confirm password do not match')
-        elif db.execute("SELECT * FROM users WHERE user = :username", username=username):
+        elif db.execute("SELECT * FROM tbl_user WHERE user_name = :username", username=username):
             return render_template('register.html', error='Username already exists')
         # encrypting password
         hash = generate_password_hash(password)
         # inserting data into database
-        db.execute("INSERT INTO users (user, password) VALUES (:username, :hash)", username=username, hash=hash)
+        db.execute("INSERT INTO tbl_user (user_name,user_email, user_password,created_at) VALUES (:username, :email, :hash,:createddate)", username=username,email=mail, hash=hash,createddate=datetime.now())
         # redirecting to login page
         return redirect(url_for('login'))
     return render_template('register.html')
@@ -47,11 +51,11 @@ def login():
             return render_template('login.html', error='Username is required')
         elif not password:
             return render_template('login.html', error='Password is required')
-        row = db.execute("SELECT * FROM users WHERE user = :username", username=username)
+        row = db.execute("SELECT * FROM tbl_user WHERE user_name = :username", username=username)
         if row:
             # validating pass
-            if check_password_hash(row[0]["password"],password):
-                userData={"username":row[0]["user"],"id":row[0]["id"]}
+            if check_password_hash(row[0]["user_password"],password):
+                userData={"username":row[0]["user_name"],"id":row[0]["id"]}
                 return render_template('home.html',userData=userData)
         return render_template('login.html', error='Incorrect credentials')
         
---
+++ bdscript.sql
@@ -0,0 +1,121 @@
+CREATE TABLE cat_state(
+ id INTEGER PRIMARY KEY
+,name TEXT NOT NULL
+,description TEXT 
+,created_at DATETIME NOT NULL
+);
+
+CREATE TABLE tbl_user(
+ id INTEGER PRIMARY KEY
+,user_name TEXT UNIQUE NOT NULL
+,user_email TEXT UNIQUE NOT NULL
+,user_password TEXT NOT NULL
+,user_photo TEXT 
+,state_id INTEGER 
+,created_at DATETIME NOT NULL
+,FOREIGN KEY (state_id) REFERENCES cat_state(id)
+);
+
+CREATE TABLE tbl_session(
+ id INTEGER PRIMARY KEY
+,user_id INTEGER NOT NULL
+,session_start DATETIME NOT NULL
+,session_end DATETIME
+,created_at DATETIME NOT NULL,
+FOREIGN KEY (user_id) REFERENCES tbl_user(id)
+);
+
+CREATE TABLE tbl_work_space(
+ id INTEGER PRIMARY KEY
+,title TEXT NOT NULL
+,topic TEXT NOT NULL
+,description TEXT NOT NULL
+,state_id INTEGER NOT NULL
+,created_at DATETIME NOT NULL
+,FOREIGN KEY (state_id) REFERENCES cat_state(id)
+);
+
+##Miembros del espacio de trabajo
+CREATE TABLE tbl_work_space_member(
+ id INTEGER PRIMARY KEY 
+,work_space_id INTEGER NOT NULL
+,user_id INTEGER NOT NULL
+,isLeader INTEGER NOT NULL
+,created_at DATETIME NOT NULL
+,FOREIGN KEY (work_space_id) REFERENCES tbl_work_space(id)
+,FOREIGN KEY (user_id) REFERENCES tbl_user(id)
+);
+
+CREATE TABLE tbl_note(
+ id INTEGER PRIMARY KEY
+,work_space_id INTEGER NOT NULL
+,title TEXT NOT NULL
+,description TEXT NOT NULL
+,state_id INTEGER NOT NULL
+,created_at DATETIME NOT NULL
+,FOREIGN KEY(work_space_id) REFERENCES tbl_work_space(id)
+,FOREIGN KEY (state_id) REFERENCES cat_state(id)
+);
+
+CREATE TABLE tbl_reminder(
+ id INTEGER PRIMARY KEY 
+,work_space_id INTEGER NOT NULL
+,title TEXT NOT NULL
+,description TEXT NOT NULL
+,reminder_date DATETIME NOT NULL 
+,state_id INTEGER NOT NULL
+,created_at DATETIME NOT NULL
+,FOREIGN KEY(work_space_id) REFERENCES tbl_work_space(id)
+,FOREIGN KEY (state_id) REFERENCES cat_state(id)
+);
+
+CREATE TABLE tbl_task(
+ id INTEGER PRIMARY KEY 
+,work_space_id INTEGER NOT NULL
+,titulo TEXT NOT NULL
+,descripcion TEXT NOT NULL
+,expired_date DATETIME 
+,state_id INTEGER NOT NULL
+,created_at DATETIME NOT NULL
+,FOREIGN KEY(work_space_id) REFERENCES tbl_work_space(id)
+,FOREIGN KEY (state_id) REFERENCES cat_state(id)
+);
+
+##Miembros que pertenecen a una tarea
+CREATE TABLE tbl_task_member(
+ id INTEGER PRIMARY KEY
+,task_id INTEGER NOT NULL
+,user_id INTEGER NOT NULL
+,FOREIGN KEY (task_id) REFERENCES tbl_task(id)
+,FOREIGN KEY (user_id) REFERENCES tbl_user(id)
+);
+
+##Actividades que componen a una tarea
+CREATE TABLE tbl_task_activity(
+ id INTEGER PRIMARY KEY
+,task_id INTEGER NOT NULL
+,activity TEXT NOT NULL
+,state_id INTEGER NOT NULL
+,user_asigned INTEGER
+,expired_date DATETIME
+,description TEXT NOT NULL
+,created_at DATETIME NOT NULL
+,FOREIGN KEY (user_asigned) REFERENCES tbl_user(id)
+,FOREIGN KEY (task_id) REFERENCES tbl_task(id)
+,FOREIGN KEY (state_id) REFERENCES cat_state(id)
+);
+
+CREATE TABLE cat_permission(
+ id INTEGER PRIMARY KEY
+,name TEXT NOT NULL
+,description TEXT
+,created_at DATETIME NOT NULL
+);
+
+CREATE TABLE tbl_work_space_permission(
+ id INTEGER PRIMARY KEY NOT NULL
+,work_space_id INTEGER NOT NULL
+,user_id INTEGER NOT NULL
+,permission_id INTEGER NOT NULL
+,FOREIGN KEY (permission_id) REFERENCES cat_permission (id)
+);
