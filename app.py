855ef3bff4eb750f7e7e798e612d2066d46ab69b
Zukhulento <luis.matus.jr@gmail.com>
2023-11-23 21:44:43 -0600

    logout and home upgrades

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_session import Session
 from cs50 import SQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Database config
 db = SQL("sqlite:///SmartPlanner.db")
@@ -36,12 +45,17 @@ def register():
         hash = generate_password_hash(password)
         # inserting data into database
         db.execute("INSERT INTO tbl_user (user_name,user_email, user_password,created_at) VALUES (:username, :email, :hash,:createddate)", username=username,email=mail, hash=hash,createddate=datetime.now())
+        # Creating the new workspace 
+        db.execute("INSERT INTO tbl_work_space (title,topic,descripcion,owner,state_id,created_at) VALUES (:title, :topic, :description,:owner, :state_id, :created_at)", title="Espacio personal",topic="Primer espacio",description="Puedes crear ahora tareas, anotaciones o recordatorios", owner =session["user_id"], state_id="1", created_at=datetime.now())
+        
         # redirecting to login page
         return redirect(url_for('login'))
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Forget any user_id
+    session.clear()
     error = None
     if request.method == 'POST':
          # validating data
@@ -55,13 +69,25 @@ def login():
         if row:
             # validating pass
             if check_password_hash(row[0]["user_password"],password):
-                userData={"username":row[0]["user_name"],"id":row[0]["id"]}
-                return render_template('home.html',userData=userData)
+                session["user_id"] = row[0]["id"]
+                session["user_name"] = row[0]["user_name"]
+                session["user_email"] = row[0]["user_email"]
+                if row[0]["user_photo"]:
+                    session["user_photo"] = row[0]["user_photo"]
+                return render_template('home.html')
         return render_template('login.html', error='Incorrect credentials')
-        
-        return redirect(url_for('login'))
     return render_template('login.html', error=error)
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
